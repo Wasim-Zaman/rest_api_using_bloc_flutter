56af0d0b6d170bb8a14927e0a01ea20d8bbfa2ee
Wasim Zaman <wasimxaman13@gmail.com>
2022-12-31 10:36:06 +0500

    finally completed properly

--- lib/blocs/image_bloc.dart
+++ lib/blocs/image_bloc.dart
@@ -8,10 +8,15 @@ class ImageBloc extends Bloc<ImageEvents, ImageStates> {
   final ImageRepository imageRepository;
 
   ImageBloc(this.imageRepository) : super(ImageLoadingState()) {
+    // loading event
     on<ImageLoadingEvent>((event, emit) => emit(ImageLoadingState()));
+    // exception event
     on<ImageExceptionEvent>(
         (event, emit) => emit(ImageExceptionState('Error Occured!')));
+    // data loaded event
     on<ImageLoadedEvent>((event, emit) async {
+      // before the data is actually fetched, we emit the loading state
+      emit(ImageLoadingState());
       try {
         final images = await imageRepository.getModel();
         emit(ImageLoadedState(images));
